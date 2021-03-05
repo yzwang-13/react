f73fc66c1a9be4ed732a8ad072722299ab82d008
yingzhengwang <yingzheng660@gmail.com>
2021-03-05 15:19:26 +1100

    fuctiona-components hook of useState and old state value copy issues

--- src/App.js
+++ src/App.js
@@ -1,41 +1,85 @@
-import React, { Component } from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
+const App = props => {
+   const [personsState, setPersonsState] =  useState({
+        persons: [
+            {name: 'Max', age: 28},
+            {name: 'Wilson', age: 29},
+            {name: 'Elevyn', age: 29}
+        ],
 
-  state = {
-    persons: [
-      {name: 'Max', age: 28},
-      {name: 'Wilson', age: 29},
-      {name: 'Elevyn', age: 29}
-    ],
-    otherState: "Some other value"
-  }
-
-  switchNameHandler = () => {
-    // console.log("was clicked");
-    // merge the old state with the new one, will not override old state.
-    this.setState({
-      persons: [
-        {name: 'AAA', age: 0},
-        {name: 'BBB', age: 0},
-        {name: 'CCC', age: 0}
-      ]
     });
-  }
 
-  render() {
+   const [otherState, setOtherState] = useState({otherState1:'Some other value for other state 1'});
+
+   const [otherState2, setOtherState2] = useState({otherState1:'Some other value for other state 2'});
+
+
+    console.log(personsState, otherState, otherState2);
+
+    const switchNameHandler = () => {
+        // console.log("was clicked");
+        // merge the old state with the new one, will not override old state.
+        setPersonsState({
+            persons: [
+                {name: 'AAA', age: 0},
+                {name: 'BBB', age: 0},
+                {name: 'CCC', age: 0}
+            ]
+        });
+    }
+
     return (
-      <div className="App">
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
-        <h1>Hi I am a react app</h1>
-      </div>
+        <div className="App">
+            <button onClick={switchNameHandler}>Switch Name</button>
+            <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
+            <Person name={personsState.persons[1].name} age={personsState.persons[1].age}/>
+            <Person name={personsState.persons[2].name} age={personsState.persons[2].age}></Person>
+            <h1>Hi I am a react app</h1>
+        </div>
     );
-  }
+
 }
 
 export default App;
+
+
+// class App extends Component {
+//
+//   state = {
+//     persons: [
+//       {name: 'Max', age: 28},
+//       {name: 'Wilson', age: 29},
+//       {name: 'Elevyn', age: 29}
+//     ],
+//     otherState: "Some other value"
+//   }
+//
+//   switchNameHandler = () => {
+//     // console.log("was clicked");
+//     // merge the old state with the new one, will not override old state.
+//     this.setState({
+//       persons: [
+//         {name: 'AAA', age: 0},
+//         {name: 'BBB', age: 0},
+//         {name: 'CCC', age: 0}
+//       ]
+//     });
+//   }
+//
+//   render() {
+//     return (
+//       <div className="App">
+//         <button onClick={this.switchNameHandler}>Switch Name</button>
+//         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+//         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+//         <Person name={this.state.persons[2].name} age={personsState.persons[2].age}></Person>
+//         <h1>Hi I am a react app</h1>
+//       </div>
+//     );
+//   }
+// }
+//
+// export default App;
