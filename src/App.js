a0c19931c855dea68abccef309cce08dad58ec26
yingzhengwang <yingzheng660@gmail.com>
2021-03-05 14:49:41 +1100

    change state

--- src/App.js
+++ src/App.js
@@ -9,16 +9,29 @@ class App extends Component {
       {name: 'Max', age: 28},
       {name: 'Wilson', age: 29},
       {name: 'Elevyn', age: 29}
-    ]
+    ],
+    otherState: "Some other value"
+  }
+
+  switchNameHandler = () => {
+    // console.log("was clicked");
+    // merge the old state with the new one, will not override old state.
+    this.setState({
+      persons: [
+        {name: 'AAA', age: 0},
+        {name: 'BBB', age: 0},
+        {name: 'CCC', age: 0}
+      ]
+    });
   }
 
   render() {
     return (
       <div className="App">
-        <button>Switch Name</button>
-        <Person name="Max" age="28"/>
-        <Person name="Wilson" age="28"/>
-        <Person name="Elevyn" age="29"><h1>This is a children</h1></Person>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
         <h1>Hi I am a react app</h1>
       </div>
     );
