6289740938ec9426d257bae3a7ace88546bac034
yingzhengwang <yingzheng660@gmail.com>
2021-03-05 16:02:06 +1100

    1.Changes happened at child component but need to be handled at parent component functions 2. 2 ways of handling changes including one less effcient way 3. passing values to handler function 4. 2 way binding

--- src/App.js
+++ src/App.js
@@ -1,85 +1,96 @@
-import React, {useState} from 'react';
+import React, {Component, useState} from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const App = props => {
-   const [personsState, setPersonsState] =  useState({
+class App extends Component {
+
+    state = {
         persons: [
             {name: 'Max', age: 28},
             {name: 'Wilson', age: 29},
             {name: 'Elevyn', age: 29}
         ],
+        otherState: "Some other value"
+    }
 
-    });
-
-   const [otherState, setOtherState] = useState({otherState1:'Some other value for other state 1'});
-
-   const [otherState2, setOtherState2] = useState({otherState1:'Some other value for other state 2'});
-
-
-    console.log(personsState, otherState, otherState2);
-
-    const switchNameHandler = () => {
+    switchNameHandler = (newName) => {
         // console.log("was clicked");
         // merge the old state with the new one, will not override old state.
-        setPersonsState({
+        this.setState({
             persons: [
-                {name: 'AAA', age: 0},
+                {name: newName, age: 0},
                 {name: 'BBB', age: 0},
                 {name: 'CCC', age: 0}
             ]
         });
     }
 
-    return (
-        <div className="App">
-            <button onClick={switchNameHandler}>Switch Name</button>
-            <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
-            <Person name={personsState.persons[1].name} age={personsState.persons[1].age}/>
-            <Person name={personsState.persons[2].name} age={personsState.persons[2].age}></Person>
-            <h1>Hi I am a react app</h1>
-        </div>
-    );
+    nameChangedHandler = (event) => {
+        this.setState({
+            persons: [
+                {name: event.target.value, age: 0},
+                {name: 'BBB', age: 0},
+                {name: 'CCC', age: 0}
+            ]
+        })
+    }
 
+    render() {
+        return (
+            <div className="App">
+                <button onClick={this.switchNameHandler}>Switch Name</button>
+                <Person clicked={this.switchNameHandler} name={this.state.persons[0].name}
+                        age={this.state.persons[0].age}
+                        changed={this.nameChangedHandler}/>
+                <Person clicked={() => this.switchNameHandler("NewName 3 Anonymous function call")}
+                        name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+                <Person clicked={this.switchNameHandler.bind(this, "NewName 4")} name={this.state.persons[2].name}
+                        age={this.state.persons[2].age}></Person>
+                <h1>Hi I am a react app</h1>
+            </div>
+        );
+    }
 }
 
 export default App;
 
 
-// class App extends Component {
+// const App = props => {
+//    const [personsState, setPersonsState] =  useState({
+//         persons: [
+//             {name: 'Max', age: 28},
+//             {name: 'Wilson', age: 29},
+//             {name: 'Elevyn', age: 29}
+//         ],
 //
-//   state = {
-//     persons: [
-//       {name: 'Max', age: 28},
-//       {name: 'Wilson', age: 29},
-//       {name: 'Elevyn', age: 29}
-//     ],
-//     otherState: "Some other value"
-//   }
-//
-//   switchNameHandler = () => {
-//     // console.log("was clicked");
-//     // merge the old state with the new one, will not override old state.
-//     this.setState({
-//       persons: [
-//         {name: 'AAA', age: 0},
-//         {name: 'BBB', age: 0},
-//         {name: 'CCC', age: 0}
-//       ]
 //     });
-//   }
 //
-//   render() {
+//    const [otherState, setOtherState] = useState({otherState1:'Some other value for other state 1'});
+//
+//    const [otherState2, setOtherState2] = useState({otherState1:'Some other value for other state 2'});
+//    console.log(personsState, otherState, otherState2);
+//
+//     const switchNameHandler = () => {
+//         // console.log("was clicked");
+//         // merge the old state with the new one, will not override old state.
+//         setPersonsState({
+//             persons: [
+//                 {name: 'AAA', age: 0},
+//                 {name: 'BBB', age: 0},
+//                 {name: 'CCC', age: 0}
+//             ]
+//         });
+//     }
+//
 //     return (
-//       <div className="App">
-//         <button onClick={this.switchNameHandler}>Switch Name</button>
-//         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-//         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-//         <Person name={this.state.persons[2].name} age={personsState.persons[2].age}></Person>
-//         <h1>Hi I am a react app</h1>
-//       </div>
+//         <div className="App">
+//             <button onClick={switchNameHandler}>Switch Name</button>
+//             <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
+//             <Person name={personsState.persons[1].name} age={personsState.persons[1].age}/>
+//             <Person name={personsState.persons[2].name} age={personsState.persons[2].age}></Person>
+//             <h1>Hi I am a react app</h1>
+//         </div>
 //     );
-//   }
-// }
 //
+// }
 // export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,8 +3,9 @@ import React from 'react';
 const person = (props) => {
     return (
         <div>
-            <h1> My name is {props.name} and I am {props.age} old</h1>
+            <p onClick={props.clicked}> My name is {props.name} and I am {props.age} old</p>
             <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name}/>
         </div>
     )
 };
