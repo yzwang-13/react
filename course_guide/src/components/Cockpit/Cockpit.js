05f37c56a787bd9b59809884ed891205727efdc9
yingzhengwang <yingzheng660@gmail.com>
2021-04-04 14:27:08 +1000

    component lifecycle in functional component

--- course_guide/src/components/Cockpit/Cockpit.js
+++ course_guide/src/components/Cockpit/Cockpit.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, {useEffect}from 'react';
 import Classes from './Cockpit.css';
 
 const cockpit = (props) => {
+
+    useEffect(()=> {
+        console.log('[Cockpit.js] useEffect')
+    },[trackingVariable1,trackingVariable2, trackingVariable3]);
 
     let btnClasses = '';
     if (props.showPerson === true){
