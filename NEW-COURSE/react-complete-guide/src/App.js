eda6938985001e8b3c08261e0c9be69403404204
yingzhengwang <yingzheng660@gmail.com>
2021-04-20 13:34:09 +1000

    new course started focusing on current react best practices

---
+++ NEW-COURSE/react-complete-guide/src/App.js
@@ -0,0 +1,46 @@
+import './App.css';
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+    const expenses = [
+        {
+            id: 'e1',
+            title: 'Toilet Paper',
+            amount: 94.12,
+            date: new Date(2020, 7, 14),
+        },
+        {id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12)},
+        {
+            id: 'e3',
+            title: 'Car Insurance',
+            amount: 294.67,
+            date: new Date(2021, 2, 28),
+        },
+        {
+            id: 'e4',
+            title: 'New Desk (Wooden)',
+            amount: 450,
+            date: new Date(2021, 5, 12),
+        },
+    ];
+
+
+    return (
+        <div>
+            <h1>Let's get started</h1>
+            <p>This is also visiable</p>
+            {expenses.map((e) => {
+                return(
+                    <ExpenseItem
+                        key={e.id}
+                        title={e.title}
+                        amount={e.amount}
+                        date={e.date}
+                    />
+                    )
+            })}
+        </div>
+    );
+}
+
+export default App;
