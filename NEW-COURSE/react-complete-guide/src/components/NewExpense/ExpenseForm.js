12b2b43deb04cfa30b20050ab87b4fba0710b265
yingzhengwang <yingzheng660@gmail.com>
2021-04-21 13:52:17 +1000

    Back to use multi state approach

--- NEW-COURSE/react-complete-guide/src/components/NewExpense/ExpenseForm.js
+++ NEW-COURSE/react-complete-guide/src/components/NewExpense/ExpenseForm.js
@@ -5,30 +5,44 @@ const ExpenseFrom = () => {
 
     // the logic is kind of the same but now it's in one state object
     // managed as one piece of state instead of three separate slices.
-    const [userInput, setUserInput] = useState({
-        enteredTitle: '',
-        enteredAmount: '',
-        enteredDate: ''
-    })
+    // const [userInput, setUserInput] = useState({
+    //     enteredTitle: '',
+    //     enteredAmount: '',
+    //     enteredDate: ''
+    // })
+
+    // we still use multi state approach
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
+
 
     const titleChangeHandler = event => {
         // need to copy old state so that other values won't get lost
-        setUserInput((preState) => {
-            return {...preState, enteredTitle: event.target.value};
-        })
+        // setUserInput((preState) => {
+        //     return {...preState, enteredTitle: event.target.value};
+        // })
+
+        setEnteredTitle(event.target.value);
     }
 
     const amountChangeHandler = event => {
-        setUserInput((preState) => {
-            return {...preState, enteredAmount: event.target.value};
-        })
+
+        // setUserInput((preState) => {
+        //     return {...preState, enteredAmount: event.target.value};
+        // })
+
+        setEnteredAmount(event.target.value);
+
 
     }
 
     const dateChangeHandler = event => {
-        setUserInput((preState) => {
-            return {...preState, enteredDate: event.target.value};
-        })
+
+        // setUserInput((preState) => {
+        //     return {...preState, enteredDate: event.target.value};
+        // })
+        setEnteredDate(event.target.value);
     }
 
     return <form>
