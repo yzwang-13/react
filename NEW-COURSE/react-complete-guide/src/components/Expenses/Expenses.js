e258c24d21ff100fff01e8cc746bfea67ab89c0e
yingzhengwang <yingzheng660@gmail.com>
2021-04-25 21:03:13 +1000

    filter challenger & 2-way binding user year selection

--- NEW-COURSE/react-complete-guide/src/components/Expenses/Expenses.js
+++ NEW-COURSE/react-complete-guide/src/components/Expenses/Expenses.js
@@ -1,18 +1,28 @@
 import ExpenseItem from "./ExpenseItem";
 import './Expenses.css';
 import Card from "../UI/Card";
+import ExpenseFilter from "./ExpenseFilter";
+import {useState} from 'react';
 
 const Expenses = (props) => {
+
+    const [selectedYear, setYear] = useState(new Date().getFullYear().toString());
+
+    const filterSelectedHandler = (event) => {
+        setYear(event.target.value)
+    }
+
     return (
         <Card className='expenses'>
+            <ExpenseFilter year={selectedYear} yearSelectedHandler={filterSelectedHandler}/>
             {props.expenses.map((e) => {
                 return (
-                    <ExpenseItem
-                        key={e.id}
-                        title={e.title}
-                        amount={e.amount}
-                        date={e.date}
-                    />
+                        <ExpenseItem
+                            key={e.id}
+                            title={e.title}
+                            amount={e.amount}
+                            date={e.date}
+                        />
                 )
             })}
         </Card>
