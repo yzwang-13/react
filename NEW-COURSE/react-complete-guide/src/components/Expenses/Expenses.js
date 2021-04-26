315f5e0569472e556c84a137ddea04367d6d17d4
yingzhengwang <yingzheng660@gmail.com>
2021-04-26 11:59:22 +1000

    extracting expensesList to a standalone component

--- NEW-COURSE/react-complete-guide/src/components/Expenses/Expenses.js
+++ NEW-COURSE/react-complete-guide/src/components/Expenses/Expenses.js
@@ -3,28 +3,14 @@ import './Expenses.css';
 import Card from "../UI/Card";
 import ExpenseFilter from "./ExpenseFilter";
 import {useState} from 'react';
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
-
-    let expensesContent = <p> No expenses found </p>;
-    if (props.expenses.length !== 0) {
-        expensesContent = props.expenses.map((e) =>
-            (
-                <ExpenseItem
-                    key={e.id}
-                    title={e.title}
-                    amount={e.amount}
-                    date={e.date}
-                />
-            ))
-    }
-
 
     return (
         <Card className='expenses'>
             <ExpenseFilter year={props.year} yearSelectedHandler={props.changeYear}/>
-            {expensesContent}
-            }
+            <ExpensesList items={props.expenses}/>
         </Card>
     )
 }
