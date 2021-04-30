47513dde58ef9d6ccf073fe26e84e1d5358398c7
yingzhengwang <yingzheng660@gmail.com>
2021-04-30 12:51:45 +1000

    inline css with the highest priority to conditionally changing styles

--- NEW-COURSE/06-styling-project/src/components/CourseGoals/CourseInput/CourseInput.js
+++ NEW-COURSE/06-styling-project/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,29 +1,41 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
 import Button from '../../UI/Button/Button';
 import './CourseInput.css';
 
 const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+    const [enteredValue, setEnteredValue] = useState('');
+    const [isValid, setValid] = useState(true);
 
-  const goalInputChangeHandler = event => {
-    setEnteredValue(event.target.value);
-  };
 
-  const formSubmitHandler = event => {
-    event.preventDefault();
-    props.onAddGoal(enteredValue);
-  };
+    const goalInputChangeHandler = event => {
+        setEnteredValue(event.target.value);
+        if (event.target.value.trim().length > 0){
+            setValid(true);
+        }
+    };
 
-  return (
-    <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
-      </div>
-      <Button type="submit">Add Goal</Button>
-    </form>
-  );
+    const formSubmitHandler = event => {
+        event.preventDefault();
+        // if a user entered nothing, simply return
+        if (enteredValue.trim().length === 0) {
+            setValid(false);
+            return;
+        }
+        props.onAddGoal(enteredValue);
+    };
+
+    return (
+        <form onSubmit={formSubmitHandler}>
+            <div className="form-control">
+                <label style={{color: !isValid ? 'red' : 'black'}}>Course Goal</label>
+                <input style={
+                    {border: !isValid ? '1px solid red' : '1px solid lightGrey',
+                    backgroundColor: !isValid ? 'salmon' : 'transparent'}} type="text" onChange={goalInputChangeHandler}/>
+            </div>
+            <Button type="submit">Add Goal</Button>
+        </form>
+    );
 };
 
 export default CourseInput;
