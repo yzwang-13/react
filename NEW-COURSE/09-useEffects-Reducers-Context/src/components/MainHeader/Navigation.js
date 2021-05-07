2d45b89c104fc100156244de4ac8ee22a92e7f84
yingzhengwang <yingzheng660@gmail.com>
2021-05-07 13:07:02 +1000

    useContext hook to listen to and fetch contextData in a component instead of context.consuemr syntax

--- NEW-COURSE/09-useEffects-Reducers-Context/src/components/MainHeader/Navigation.js
+++ NEW-COURSE/09-useEffects-Reducers-Context/src/components/MainHeader/Navigation.js
@@ -1,34 +1,31 @@
-import React from 'react';
+import React, {useContext} from 'react';
 
 import classes from './Navigation.module.css';
 import AuthContext from "../../context/auth-context";
 
-const Navigation = (props) => {
-    return (
-        <AuthContext.Consumer>
-            {(contextData) => {
-                return (<nav className={classes.nav}>
-                    <ul>
-                        {contextData.isLoggedIn && (
-                            <li>
-                                <a href="/">Users</a>
-                            </li>
-                        )}
-                        {contextData.isLoggedIn && (
-                            <li>
-                                <a href="/">Admin</a>
-                            </li>
-                        )}
-                        {contextData.isLoggedIn && (
-                            <li>
-                                <button onClick={props.onLogout}>Logout</button>
-                            </li>
-                        )}
-                    </ul>
-                </nav>)
-            }}
-        </AuthContext.Consumer>
+const Navigation = () => {
 
+    const contextData = useContext(AuthContext);
+
+    return (<nav className={classes.nav}>
+            <ul>
+                {contextData.isLoggedIn && (
+                    <li>
+                        <a href="/">Users</a>
+                    </li>
+                )}
+                {contextData.isLoggedIn && (
+                    <li>
+                        <a href="/">Admin</a>
+                    </li>
+                )}
+                {contextData.isLoggedIn && (
+                    <li>
+                        <button onClick={contextData.onLogout}>Logout</button>
+                    </li>
+                )}
+            </ul>
+        </nav>
     );
 };
 
