030d9a31ac350b212f7bf48c5de1b5fd5fd49c62
yingzhengwang <yingzheng660@gmail.com>
2021-05-09 01:05:45 +1000

    continue working on meals and meal function

--- NEW-COURSE/11-food-order-app/src/components/Meals/Meal/Meal.js
+++ NEW-COURSE/11-food-order-app/src/components/Meals/Meal/Meal.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import classes from './Meal.module.css';
 
 const Meal = props => {
+
+    const [enteredAmount, updateEnteredAmount] = useState(1);
+
+
+    const addAmountHandler = (event) => {
+        if (event.target.type === 'number') {           // coming from input
+            updateEnteredAmount(+event.target.value);
+            return
+        }
+    }
+
+    const submitNewOrder = () => {
+        props.updateOrders({id: props.id, amount: enteredAmount});
+    }
+
     return (
         <li className={classes.meal}>
-            <h3>Sushi</h3>
-            <p className={classes.description}>Finest fish and veggies</p>
-            <p className={classes.price}>$22.99</p>
+            <h3>{props.name}</h3>
+            <p className={classes.description}>{props.description}</p>
+            <p className={classes.price}>${props.price}</p>
             <p>Amount</p>
-            <input type='amount' min={0} />
-            <button>+Add</button>
+            <input type="number" onChange={addAmountHandler} value={enteredAmount} min={1} step="any"/>
+            <button onClick={submitNewOrder}>+Add</button>
         </li>
     )
 }
--- NEW-COURSE/11-food-order-app/src/components/Meals/Meals.js
+++ NEW-COURSE/11-food-order-app/src/components/Meals/Meals.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Meal from "./Meal/Meal";
 import classes from './Meals.module.css';
 
 const Meals = props => {
 
+    const [orders, updateOrders] = useState({});
+
+    const ordersUpdateHandler = newOrder => {
+        updateOrders(preOrders => {
+            if (preOrders[newOrder.id]) {
+                preOrders[newOrder.id] += newOrder.amount
+                return preOrders;
+            }else {
+                preOrders[newOrder.id] = newOrder.amount;
+                return preOrders
+            }
+        });
+
+        console.log(orders);
+    }
+
+
     return (
-        <ul className={classes.meals}>
-            <Meal/>
-        </ul>
+        <div className={classes.meals}>
+            <ul>
+                {props.meals.map(meal => <Meal updateOrders={ordersUpdateHandler} key={meal.id} id={meal.id} price={meal.price} description={meal.description} name={meal.name} />)}
+            </ul>
+        </div>
     )
 }
 
