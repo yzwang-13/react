9803b6bd2ff12ddde428aa0c76df8975b833c0fb
yingzhengwang <yingzheng660@gmail.com>
2021-05-14 11:51:40 +1000

    sending get requests to backend

--- NEW-COURSE/14-http-requests/src/App.js
+++ NEW-COURSE/14-http-requests/src/App.js
@@ -1,34 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
 
-  return (
-    <React.Fragment>
-      <section>
-        <button>Fetch Movies</button>
-      </section>
-      <section>
-        <MoviesList movies={dummyMovies} />
-      </section>
-    </React.Fragment>
-  );
+    const [movies, setMovies] = useState([]);
+
+    async function fetchMoviesHandler() {
+        const data = (await fetch('https://swapi.dev/api/films/', {
+            method: 'GET',
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })).json().then(data => {
+            console.log(data.results);
+
+            // do the transform data before displaying to match our format
+            // transform the data to only what we need
+            const movies = data.results.map( movie => {
+                return {
+                    id: movie.episode_id,
+                    releaseDate: movie.release_date,
+                    openingText: movie.opening_crawl,
+                    title: movie.title
+                }
+            })
+            setMovies(movies);
+        });
+        // const result = data.json().then( data => console.log(data));
+    }
+
+    return (
+        <React.Fragment>
+            <section>
+                <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+            </section>
+            <section>
+                <MoviesList movies={movies} />
+            </section>
+        </React.Fragment>
+    );
 }
 
 export default App;
