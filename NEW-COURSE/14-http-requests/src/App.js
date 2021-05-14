f0aca4c9f29354e0e19ac89a28efb6b1a7357f3b
yingzhengwang <yingzheng660@gmail.com>
2021-05-14 12:47:53 +1000

    make sure function gets run when the component is being created and make sure the function gets run again only when needed and not gets run when it does not need to run

--- NEW-COURSE/14-http-requests/src/App.js
+++ NEW-COURSE/14-http-requests/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState, useCallback} from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -8,33 +8,52 @@ function App() {
 
     const [movies, setMovies] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
 
-
-    async function fetchMoviesHandler() {
+    const fetchMoviesHandler = useCallback( async () => {
         setIsLoading(true);
-        const response = await fetch('https://swapi.dev/api/films/', {
-            method: 'GET',
-            mode: 'cors',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-        });
-        const data = await response.json();
-
-        // do the transform data before displaying to match our format
-        // transform the data to only what we need
-        const movies = data.results.map(movie => {
-            return {
-                id: movie.episode_id,
-                releaseDate: movie.release_date,
-                openingText: movie.opening_crawl,
-                title: movie.title
+        setError(null);
+        try {
+            // error url https://swapi.dev/api/film/
+            const response = await fetch('https://swapi.dev/api/films/', {
+                method: 'GET',
+                mode: 'cors',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            });
+            console.log(response);
+            if (!response.ok){
+                throw new Error('Ahh, shit Something went wrong');
             }
-        })
-        setMovies(movies);
+
+            const data = await response.json();
+
+            // do the transform data before displaying to match our format
+            // transform the data to only what we need
+            const movies = data.results.map(movie => {
+                return {
+                    id: movie.episode_id,
+                    releaseDate: movie.release_date,
+                    openingText: movie.opening_crawl,
+                    title: movie.title
+                }
+            })
+            setMovies(movies);
+        } catch (e) {
+            // updating the error state
+            setError(e.message);
+        }
+
         setIsLoading(false);
-        // const result = data.json().then( data => console.log(data));
-    }
+
+
+
+    }, []);
+
+    useEffect( () => {
+        fetchMoviesHandler();
+    }, [fetchMoviesHandler])
 
     return (
         <React.Fragment>
@@ -42,9 +61,11 @@ function App() {
                 <button onClick={fetchMoviesHandler}>Fetch Movies</button>
             </section>
             <section>
-                {!isLoading && movies.length >0 && <MoviesList movies={movies}/>}
-                {!isLoading && movies.length === 0 && <p>Found No Movies!</p>}
+                {!isLoading && movies.length > 0 && <MoviesList movies={movies}/>}
+                {!isLoading && movies.length === 0 && !error && <p>Found No Movies! Try to click the button to fetch movies</p>}
+                {!isLoading && error && <p>{error}</p>}
                 {isLoading && <Spinner/>}
+
             </section>
         </React.Fragment>
     );
