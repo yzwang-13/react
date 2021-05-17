cf5cb682a57200d1e6f2b3a17af75abef7c8886c
yingzhengwang <yingzheng660@gmail.com>
2021-05-17 17:50:04 +1000

    create a new redux store and start connecting with react

---
+++ NEW-COURSE/18-Redux-react/src/store/index.js
@@ -0,0 +1,14 @@
+import redux from 'redux';
+
+const counterReducer = (oldState = {counter: 0}, action) => {
+    if (action.type === 'increment'){
+        return {counter: oldState.counter + 1};
+    } else if (action.type === 'decrement'){
+        return {counter: oldState.counter - 1};
+    }
+    return oldState;
+}
+
+const store = redux.createStore(counterReducer);
+
+
