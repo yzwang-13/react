c41a74c2cc62033f753f89811f84f1a2ba7e35b7
yingzhengwang <yingzheng660@gmail.com>
2021-05-25 15:29:31 +1000

    protect the route against user access

--- NEW-COURSE/22-Authentication/src/App.js
+++ NEW-COURSE/22-Authentication/src/App.js
@@ -1,26 +1,38 @@
-import { Switch, Route } from 'react-router-dom';
+import {Switch, Route, Redirect} from 'react-router-dom';
 
 import Layout from './components/Layout/Layout';
 import UserProfile from './components/Profile/UserProfile';
 import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
+import AuthContenxt from "./store/auth-context";
+import {useContext} from "react";
 
 function App() {
-  return (
-    <Layout>
-      <Switch>
-        <Route path='/' exact>
-          <HomePage />
-        </Route>
-        <Route path='/auth'>
-          <AuthPage />
-        </Route>
-        <Route path='/profile'>
-          <UserProfile />
-        </Route>
-      </Switch>
-    </Layout>
-  );
+
+    const authcontext = useContext(AuthContenxt);
+
+    return (
+        <Layout>
+            <Switch>
+                <Route path='/' exact>
+                    <HomePage/>
+                </Route>
+                {
+                    !authcontext.isLoggedIn &&
+                    <Route path='/auth'>
+                        <AuthPage/>
+                    </Route>
+                }
+                <Route path='/profile'>
+                    {authcontext.isLoggedIn && <UserProfile/>}
+                    {!authcontext.isLoggedIn && <Redirect to='/auth'/>}
+                </Route>
+                <Route path='*'>
+                    <Redirect to='/'/>
+                </Route>
+            </Switch>
+        </Layout>
+    );
 }
 
 export default App;
--- NEW-COURSE/22-Authentication/src/store/auth-context.js
+++ NEW-COURSE/22-Authentication/src/store/auth-context.js
@@ -14,6 +14,7 @@ export const AuthContextProvider = (props) => {
 
     const loginHandler = (token) => {
         setToken(token)
+
     };
 
     const logoutHandler = () => {
