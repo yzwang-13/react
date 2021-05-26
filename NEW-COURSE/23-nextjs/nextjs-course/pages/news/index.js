6176481ffbdfe089ebc577eebed4bab6094f1647
yingzhengwang <yingzheng660@gmail.com>
2021-05-26 14:29:21 +1000

    nextjs basics

--- NEW-COURSE/23-nextjs/nextjs-course/pages/news/index.js
+++ NEW-COURSE/23-nextjs/nextjs-course/pages/news/index.js
@@ -1,8 +1,31 @@
 // our-domain.com/news
+import React from 'react';
+import Link from 'next/link';
 
 
 const NewsPage = () => {
-    return <h1>The News Page</h1>
+    return (
+        <React.Fragment>
+            <h1>The News Page</h1>
+            <ul>
+                <li>
+                    <Link href='/news/next-js'>
+                        Nextjs is a great framework
+                    </Link>
+                </li>
+                <li>
+                    <Link href='/news/something'>
+                        Something else
+                    </Link>
+                </li>
+                <li>
+                    <Link href='/news/today'>
+                        Today is a great day
+                    </Link>
+                </li>
+            </ul>
+        </React.Fragment>
+    )
 }
 
 export default NewsPage;
